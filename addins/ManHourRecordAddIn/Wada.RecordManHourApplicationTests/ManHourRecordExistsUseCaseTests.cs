3e5b00d55b58edd3e0251d53204fe3ac2409c9ca
Matazoro-Kemtari <k_hirano@wadass.com>
2023-03-14 08:16:12 +0900

    実績登録UseCaseテスト復活

---
+++ addins/ManHourRecordAddIn/Wada.RecordManHourApplicationTests/ManHourRecordExistsUseCaseTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Wada.ManHourRecordService;
+using Wada.ManHourRecordService.AttendanceAggregation;
+
+namespace Wada.RecordManHourApplication.Tests
+{
+    [TestClass()]
+    public class ManHourRecordExistsUseCaseTests
+    {
+        [TestMethod()]
+        public async Task 正常系_実績がないときは正常終了すること()
+        {
+            // given
+            // when
+            Mock<IAttendanceRepository> mock_attendance = new();
+            mock_attendance.Setup(x => x.FindByEmployeeNumberAndAchievementDateAsync(
+                It.IsAny<uint>(), It.IsAny<DateTime>()))
+                .ThrowsAsync(new AttendanceAggregationException());
+
+            IManHourRecordExistsUseCase useCase =
+                new ManHourRecordExistsUseCase(mock_attendance.Object);
+
+            var attendancePram = TestAttendanceParamFactory.Create();
+            Task target() => useCase.ExecuteAsync(attendancePram);
+
+            // then
+            await target();
+        }
+
+        [TestMethod]
+        public async Task 異常系_実績があるときは例外を返すこと()
+        {
+            Mock<IAttendanceRepository> mock_attendance = new();
+            mock_attendance.Setup(x => x.FindByEmployeeNumberAndAchievementDateAsync(
+                It.IsAny<uint>(), It.IsAny<DateTime>()));
+
+            IManHourRecordExistsUseCase useCase =
+                new ManHourRecordExistsUseCase(mock_attendance.Object);
+
+            var attendancePram = TestAttendanceParamFactory.Create();
+            Task target() => useCase.ExecuteAsync(attendancePram);
+
+            // then
+            var ex = await Assert.ThrowsExceptionAsync<ManHourRecordExistsException>(target);
+        }
+    }
+}
